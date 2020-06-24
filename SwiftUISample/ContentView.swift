ac8acfb12713a94e8426f7a556028e4b597ddc7b
keerthiv <keerthi@sparkouttech.com>
2020-06-24 10:41:48 +0530

    Initial Commit

---
+++ SwiftUISample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISample
+//
+//  Created by Hxtreme on 24/06/20.
+//  Copyright © 2020 Hxtreme. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
